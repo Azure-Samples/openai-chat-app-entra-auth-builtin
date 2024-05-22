4b45e3486fa5d04b438d7e0f2b3ecb6622def90b
Pamela Fox <pamela.fox@gmail.com>
2024-05-22 14:45:07 +0000

    Port to Chat Protocol

--- src/quartapp/chat.py
+++ src/quartapp/chat.py
@@ -52,6 +52,10 @@ async def configure_openai():
             client_args["azure_ad_token_provider"] = azure.identity.aio.get_bearer_token_provider(
                 default_credential, "https://cognitiveservices.azure.com/.default"
             )
+        if not os.getenv("AZURE_OPENAI_ENDPOINT"):
+            raise ValueError("AZURE_OPENAI_ENDPOINT is required for Azure OpenAI")
+        if not os.getenv("AZURE_OPENAI_CHATGPT_DEPLOYMENT"):
+            raise ValueError("AZURE_OPENAI_CHATGPT_DEPLOYMENT is required for Azure OpenAI")
         bp.openai_client = openai.AsyncAzureOpenAI(
             api_version=os.getenv("AZURE_OPENAI_API_VERSION") or "2024-02-15-preview",
             azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
@@ -92,7 +96,6 @@ def extract_username(headers, default_username="You"):
 
     token = json.loads(base64.b64decode(headers.get("X-MS-CLIENT-PRINCIPAL")))
     claims = {claim["typ"]: claim["val"] for claim in token["claims"]}
-    current_app.logger.info(f"Claims: {claims}")
     return claims.get("name", default_username)
 
 
@@ -102,7 +105,7 @@ async def index():
     return await render_template("index.html", username=username)
 
 
-@bp.post("/chat")
+@bp.post("/chat/stream")
 async def chat_handler():
     request_messages = (await request.get_json())["messages"]
 
@@ -121,7 +124,9 @@ async def chat_handler():
         )
         try:
             async for event in await chat_coroutine:
-                yield json.dumps(event.model_dump(), ensure_ascii=False) + "\n"
+                event_dict = event.model_dump()
+                if event_dict["choices"]:
+                    yield json.dumps(event_dict["choices"][0], ensure_ascii=False) + "\n"
         except Exception as e:
             current_app.logger.error(e)
             yield json.dumps({"error": str(e)}, ensure_ascii=False) + "\n"
